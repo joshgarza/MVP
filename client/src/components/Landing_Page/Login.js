54439edfb0a6baba2b78df388752c52a6f12110b
Josh <josh@sf-iron.com>
2023-03-27 16:53:42 -0700

    Restructured workout builder. Can still add and remove exercises, but now can add sets within each exercise and set each set's Reps and RIR.

--- client/src/components/Landing_Page/Login.js
+++ client/src/components/Landing_Page/Login.js
@@ -1,89 +1,115 @@
 import { useState } from 'react';
 
 
-//how do we handle wave loading????
-
-const workout = {
-  'Alex A.': {
-    'Tue Mar 21 2023': [
-      {
-        exercise: '',
-        reps: '',
-        rpe: '',
-        sets: '',
-        weight: '',
-        backoffPercent: '',
-        e1rmPercent: '',
-      },
-    ]
-  }
-}
-
 const WorkoutBuilder = () => {
+  const structure =
+    {
+      exercise: '',
+      sets: [
+        {
+          reps: '',
+          rir: '',
+        }
+      ]
+    };
+
+  const setStructure = {
+    reps: '',
+    rir: '',
+  }
   const [workout, setWorkout] = useState();
   const [grid, setGrid] = useState([]);
 
-  const assignmentForm = (rowIdx, colIdx) => {
-    let letter = String.fromCharCode(65 + grid[rowIdx][colIdx]);
-    console.log(grid[rowIdx][colIdx])
-
+  const assignmentForm = (rowIdx) => {
+    const slot = grid[rowIdx];
     return (
-      <div className="flex gap-2 items-center justify-center">
-        {letter}{colIdx + 1}
-        <label>Exercise</label>
-        <input type="text" className="border"></input>
-        <button className="rounded-2xl bg-slate-300 p-2 m-2" onClick={() => {
-          extendExercise(rowIdx)
-        }}>
-          Extend exercise
-        </button>
-        <button className="rounded-2xl bg-slate-300 p-2 m-2" onClick={() => {
-          removeExtension(rowIdx, colIdx)
-        }}>
-          Remove extension
-        </button>
+      <div key={rowIdx} className="relative rounded-xl overflow-auto">
+        <div>
+          <label>Exercise</label>
+          <input type="text" className="border" name="exercise" value={slot.exercise} onChange={(e) => updateExercise(e, rowIdx)}></input>
+        </div>
+        <div className="shadow-sm overflow-hidden my-2">
+          <table className="border-collapse table-auto w-full text-sm">
+            <thead>
+              <tr>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Set</th>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Reps</th>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">RIR</th>
+              </tr>
+            </thead>
+            <tbody className="bg-white dark:bg-slate-800">
+              {slot.sets.map((set, setIdx) => {
+                return (
+                  <tr key={setIdx}>
+                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{setIdx + 1}</td>
+                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                      <input type="number" name="reps" value={set.reps} onChange={(e) => updateSet(e, rowIdx, setIdx)}></input>
+                    </td>
+                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                      <input type="number" name="rir" value={set.rir} onChange={(e) => updateSet(e, rowIdx, setIdx)}></input>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <button className="m-2 rounded-xl bg-slate-200 p-2" onClick={() => {
+            removeExercise(rowIdx);
+          }}>Remove Exercise</button>
+          <button className="m-2 rounded-xl bg-slate-200 p-2" onClick={() => {
+            addSet(rowIdx);
+          }}>Add Set</button>
+        </div>
       </div>
     )
   }
 
-  const extendExercise = (rowIdx) => {
-    let copyGrid = [...grid];
-    copyGrid[rowIdx].push(rowIdx)
-    setGrid(copyGrid)
-  }
-
   const addExercise = () => {
     let copyGrid = [...grid];
-    copyGrid.push([grid.length])
-    setGrid(copyGrid)
-  }
-
-  const removeExtension = (rowIdx, colIdx) => {
-    // splice out element and update grid
-    let copyGrid = [...grid];
-
-    if (copyGrid[rowIdx].length === 1) {
-      console.log(rowIdx)
-      copyGrid.splice(rowIdx, 1)
-    } else {
-      copyGrid[rowIdx].splice(colIdx, 1);
-    }
+    copyGrid.push({...structure})
     setGrid(copyGrid)
   }
 
   const removeExercise = (rowIdx) => {
-    // splice out element and update grid
+    let copyGrid = [...grid];
+    copyGrid.splice(rowIdx, 1);
+    setGrid(copyGrid)
+  }
+
+  const updateExercise = (e, rowIdx) => {
+    const { name, value } = e.target;
+    const copyGrid = [...grid];
+
+    copyGrid[rowIdx][name] = value;
+    setGrid(copyGrid)
+  }
+
+  const addSet = (rowIdx) => {
+    let copyGrid = [...grid];
+    console.log(structure)
+    copyGrid[rowIdx].sets.push({...setStructure})
+
+    setGrid(copyGrid)
+  }
+
+  const updateSet = (e, rowIdx, setIdx) => {
+    const { name, value } = e.target;
+    const copyGrid = [...grid];
+
+    copyGrid[rowIdx].sets[setIdx][name] = value;
+
+    setGrid(copyGrid);
   }
 
   return (
-    <div className="grid overflow-scroll">
-      {grid.map((row, i) => {
-        return grid[i].map((col, j) => {
-          return (
-            assignmentForm(i, j)
-          )
-        })
-      })}
+    <div className="grid items-center justify-center">
+      <div>
+        {grid.map((slot, i) => {
+          return assignmentForm(i)
+        })}
+      </div>
       <button className="rounded-2xl bg-slate-300 p-2" onClick={() => {
         addExercise()
       }}>Add new exercise</button>
@@ -91,30 +117,4 @@ const WorkoutBuilder = () => {
   )
 }
 
-export default WorkoutBuilder
-
-// Drag and drop order. As click subExercise or new exercise, append exercise order list.
-
-// On submit, send workout object and exercise order
-
-// {
-//   Date: {
-//     workoutOrder: [0,1],
-//     workouts: [
-//      {
-//        Assignment: {
-//           exerciseOrder: [0,2,1,3,5,4],
-//           exercises: [
-//             {
-//               exercise: “squat”,
-
-//             },
-//           ],
-//         },
-//       },
-//       {
-//         Exercise: “run”,
-//       }
-//     ]
-//   }
-// }
+export default WorkoutBuilder;
