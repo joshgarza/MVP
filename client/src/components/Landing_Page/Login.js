2f026d2d5f485d9a479cadf829753a475f50bd8c
Josh <josh@sf-iron.com>
2023-03-27 17:16:38 -0700

    Added submit feature that currently logs the data to be sent

--- client/src/components/Landing_Page/Login.js
+++ client/src/components/Landing_Page/Login.js
@@ -9,6 +9,7 @@ const WorkoutBuilder = () => {
         {
           reps: '',
           rir: '',
+          backoffPercent: '',
         }
       ]
     };
@@ -16,6 +17,7 @@ const WorkoutBuilder = () => {
   const setStructure = {
     reps: '',
     rir: '',
+    backoffPercent: '',
   }
   const [workout, setWorkout] = useState();
   const [grid, setGrid] = useState([]);
@@ -26,7 +28,7 @@ const WorkoutBuilder = () => {
       <div key={rowIdx} className="relative rounded-xl overflow-auto">
         <div>
           <label>Exercise</label>
-          <input type="text" className="border" name="exercise" value={slot.exercise} onChange={(e) => updateExercise(e, rowIdx)}></input>
+          <input type="text" className="border px-1" name="exercise" value={slot.exercise} onChange={(e) => updateExercise(e, rowIdx)}></input>
         </div>
         <div className="shadow-sm overflow-hidden my-2">
           <table className="border-collapse table-auto w-full text-sm">
@@ -35,6 +37,7 @@ const WorkoutBuilder = () => {
                 <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Set</th>
                 <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Reps</th>
                 <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">RIR</th>
+                <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-right">Backoff Percent</th>
               </tr>
             </thead>
             <tbody className="bg-white dark:bg-slate-800">
@@ -43,10 +46,14 @@ const WorkoutBuilder = () => {
                   <tr key={setIdx}>
                     <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{setIdx + 1}</td>
                     <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                      <input type="number" name="reps" value={set.reps} onChange={(e) => updateSet(e, rowIdx, setIdx)}></input>
+                      <input className="px-1" type="number" name="reps" value={set.reps} onChange={(e) => updateSet(e, rowIdx, setIdx)}></input>
                     </td>
-                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                      <input type="number" name="rir" value={set.rir} onChange={(e) => updateSet(e, rowIdx, setIdx)}></input>
+                    <td className="border-b border-slate-100 dark:border-slate-700 w-full p-4 pl-8 text-slate-500 dark:text-slate-400">
+                      <input className="px-1 w-full" type="number" step="1" min="0" max="5" name="rir" value={set.rir} onChange={(e) => updateSet(e, rowIdx, setIdx)}></input>
+                    </td>
+                    <td className="flex border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                      <input className="px-1 text-right" type="number" name="backoffPercent" value={set.backoffPercent} onChange={(e) => updateSet(e, rowIdx, setIdx)}></input>
+                      <div>%</div>
                     </td>
                   </tr>
                 )
@@ -103,8 +110,15 @@ const WorkoutBuilder = () => {
     setGrid(copyGrid);
   }
 
+  const submitWorkout = () => {
+    console.log(grid)
+  }
+
   return (
     <div className="grid items-center justify-center">
+      <button className="rounded-2xl bg-slate-300 p-2" onClick={() => {
+        submitWorkout()
+      }}>Submit Workout</button>
       <div>
         {grid.map((slot, i) => {
           return assignmentForm(i)
