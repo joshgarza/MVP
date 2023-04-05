84b9a57aa8ceb29114964e4a5d9b7d1d598a49de
Josh <josh@sf-iron.com>
2023-04-04 22:08:11 -0700

    Added socket.io library and set up initial connection.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
-const express = require('express');
-const router = require('./routes.js')
-const app = express();
+const express = require("express");
+const { Server } = require("socket.io");
 
+const cors = require("cors");
+const http = require("http");
+
+const router = require("./routes.js");
+
+const app = express();
+const server = app.listen(5001, () =>
+  console.log("Server started on port 5001")
+);
+const socket = http.createServer(app);
+
+const corsOptions = {
+  origin: "http://localhost:3000", // client port
+  credentials: true,
+};
+
+app.use("/api", router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
-app.use('/api', router)
+const io = new Server(socket, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+}).listen(server);
 
-app.listen(5001, () => {
-  console.log('Server started on port 5001')
-})
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+});
